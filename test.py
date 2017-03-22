d737414e8a3a61f2d0a102376ed67e1b1130d7dd
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2017-03-22 18:53:43 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,26 @@
 import time
 import gym
 import numpy as np
+import decimal
 
 env = gym.make('CartPole-v0')
 q_table = [] #Where each line of this Q-table is state, action 1, action 2, Q-value 1, Q-value 2
 #Learning parameters
-learning_rate = 0.01
+learning_rate = 0.1
 gamma = 0.99
 r_init = 20
 
 t = 0
 i_episode = 0
 benchmark = 75
-num_past = 0
 
-while True:
-    if i_episode % 100 == 0:
+while i_episode < 1000:
+    if i_episode % 500 == 0:
         print(q_table)
-    #if t > benchmark:
-    #    num_past += 1
+        print(len(q_table))
     observation = env.reset()
-    #observation = [round(observation[i],2) for i in observation]
+    observation = [round(observation[i],3) for i in range(len(observation))]
+    observation = [observation[2],observation[3]]
     for t in range(2000): #For each timestep t in episode i_episode...
         found = False
         env.render()
@@ -31,7 +31,8 @@ while True:
                 if q_table[i][3] == q_table[i][4]:
                     action = env.action_space.sample()
                 else:
-                    action = (np.argmax([q_table[i][3],q_table[i][4]]) - 3) #Action is either 0 or 1
+                    #action = (np.argmax([q_table[i][3],q_table[i][4]]) - 3) #Action is either 0 or 1
+                    action = 1
                     q_t = q_table[i][action+3] #Q-value of the action at state s_t
                 s_t = i
         else: #If not in Q-table, sample random action
@@ -43,8 +44,8 @@ while True:
                 a_t = q_table[s_t][action+3]
         found = False
         observation, reward, done, info = env.step(action) #Take action and observe reward
-        #observation = [round(observation[i],2) for i in observation]
-        #print("%s observed with %s reward" % (observation, reward))
+        observation = [round(observation[i],3) for i in range(len(observation))]
+        observation = [observation[2],observation[3]]
         #Find location of new state s_t+1 (s_tt) in Q-table
         for j in range(len(q_table)):
             if np.array_equal(q_table[j][0],observation):
@@ -60,6 +61,3 @@ while True:
             print("Episode {} finished after {} timesteps".format(i_episode,t))
             i_episode += 1
             break
-print("SUCCESSSSSS (only %s though)" % benchmark)
-print(q_table)
-print(len(q_table))
